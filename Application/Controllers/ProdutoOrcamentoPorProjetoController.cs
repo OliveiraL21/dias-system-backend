d545e48c9c32cc7df91ca247aa5710b066066623
OliveiraL21 <lucasoliveira508@gmail.com>
2025-07-23 21:43:13 -0300

    finalizando controller de produtoOrcamento

--- Application/Controllers/ProdutoOrcamentoPorProjetoController.cs
+++ Application/Controllers/ProdutoOrcamentoPorProjetoController.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Services.ProdutoOrcamento;
+﻿using Domain.Dtos.ProdutoOrcamento;
+using Domain.Services.ProdutoOrcamento;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Net;
@@ -31,6 +32,95 @@ namespace Application.Controllers
                 var result = await _service.Delete(id);
                 return Ok(result);
             } catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("details/{id}")]
+        public async Task<IActionResult>Details(Guid id)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var result = await _service.Get(id);
+
+                return Ok(result);
+
+            } catch(Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("list")]
+        public async Task<IActionResult> List()
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var result = await _service.GetAll();
+
+                return Ok(result);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> Create([FromBody] ProdutoOrcamentoProjetoDtoCreate produto)
+        {
+            try
+            {
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var result = await _service.Create(produto);
+
+                return Ok(result);
+
+            } catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("update/{id}")]
+        public async Task<IActionResult>Update(Guid id, [FromBody] ProdutoOrcamentoProjetoDtoUpdate produto)
+        {
+            try
+            {
+
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var result = await _service.Update(id,produto);
+
+                return Ok(result);
+
+            }
+            catch (Exception ex)
             {
                 return StatusCode((int)HttpStatusCode.InternalServerError, ex.Message);
             }
